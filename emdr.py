6afc482c7a4101cfabc9af2bf6828f4c13480166
Ryan Holmes <holmes.ryan.90@gmail.com>
2013-04-26 21:02:43 +0000

    refined server reconnects, added server looping and other tweaks here and there. I would say this commit marks the beginning of a functional service

--- emdr.py
+++ emdr.py
@@ -1,73 +1,65 @@
 #!/usr/bin/env python2
-# This can be replaced with the built-in json module, if desired.
 import logging
 import simplejson
-import json
 from xml.utils.iso8601 import parse
-import datetime
-import MySQLdb
 from datetime import datetime
 import gevent
 from gevent.pool import Pool
 from gevent import monkey; gevent.monkey.patch_all()
 import zmq.green as zmq
 import zlib
-import pylibmc
-import operator
 import sys
 import redis
 
-# The maximum number of greenlet workers in the greenlet pool. This is not one
-# per processor, a decent machine can support hundreds or thousands of greenlets.
-# I recommend setting this to the maximum number of connections your database
-# backend can accept, if you must open one connection per save op.
+# The maximum number of greenlet workers in the greenlet pool.
 MAX_NUM_POOL_WORKERS = 500
 
-DEBUG   = False
-REGIONS = 'regions.json' #set this to a file containing JSON of regions to filter against
-VERSION = 1 # Change every time format for Redis values changes
+DEBUG   = False          # prints out a bunch of info.
+REGIONS = 'regions.json' # set this to a file containing JSON of regions or False for all regions
+VERSION = 1              # Change every time format for Redis values changes
 
-REQUEST_TIMEOUT = 2000 # 5s
-RELAY_RETRIES   = 10 # retries per RELAY
-MAX_RETRIES     = False # set to int if you want to terminate after x retries
-RELAYS = [
+REQUEST_TIMEOUT = 1500  # 1.5s
+RELAY_RETRIES   = 5     # retries per relay
+MAX_RETRIES     = False # set to an int if you want to terminate script after x retries
+RELAYS = [              # top relay will be primary
     'tcp://relay-us-east-1.eve-emdr.com:8050',
-    'tcp://relay-us-central-1.eve-emdr.com:8050'
+    'tcp://relay-us-central-1.eve-emdr.com:8050',
     'tcp://relay-us-west-1.eve-emdr.com:8050'
 ]
 
-NUM_RELAYS = len(RELAYS);
-
-ORDERS  = True
+ORDERS  = True  # collect information on orders
 HISTORY = False # not implemented
 
-## todo: customizable key format
+##////////////////
+## END CONFIG
+##////////////////
+
+NUM_RELAYS = len(RELAYS);
 redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 class Printer():
     """
     Print things to stdout on one line dynamically
     """
- 
     def __init__(self,data):
         sys.stdout.write("\r\x1b[K"+data.__str__())
         sys.stdout.flush()
 
         
 def main():
-    """
-    The main flow of the application.
-    """
-
-    global s # increments every time we have an already UTD cache
+    global utd      # there should be a better way to do these than with globals
     
+    retry_relay = 0 # retry count per relay
+    retry_max   = 0 # retry count total
+    relay       = 0 # current relay in use
+    utd         = 0 # increments every time we have an already UTD cache
+    cont        = True # flag to exit out of loop
+    
+    # Relay connection
     context = zmq.Context()
     client = context.socket(zmq.SUB)
-    # Connect to the first publicly available relay.
-    client.connect(RELAYS[0])
-
-    # Disable filtering.
-    client.setsockopt(zmq.SUBSCRIBE, "")
+    client.connect(RELAYS[relay])
+    client.setsockopt(zmq.SUBSCRIBE, "") # Disable filtering.
 
     poll = zmq.Poller()
     poll.register(client, zmq.POLLIN)
@@ -77,60 +69,60 @@ def main():
     
     print("Consumer daemon started, waiting for jobs...")
     print("Worker pool size: %d" % greenlet_pool.size)
-    s = 0
-    while True:
-        # Since client.recv() blocks when no messages are available,
-        # this loop stays under control. If something is available and the
-        # greenlet pool has greenlets available for use, work gets done.
-        
+
+    while cont:
         socks = dict(poll.poll(REQUEST_TIMEOUT))
-        if socks.get(client) == zmq.POLLIN:
+        if socks.get(client) == zmq.POLLIN: # message recieved within timeout
             greenlet_pool.spawn(worker, client.recv())
-            output = "%d/%d orders processed, %d skipped due to up-to-date cache" % (int(redis.get('emdr-total-saved')),int(redis.get('emdr-total-processed')),s)
-            Printer(output)
+            output = "%d/%d orders processed, %d skipped due to up-to-date cache" % \
+                     (int(redis.get('emdr-total-saved')), \
+                     int(redis.get('emdr-total-processed')), \
+                     utd)
+            Printer(output) # update output to stdout
             redis.incr('emdr-total-processed')
         else:
-            print "W: No response from server, retrying..."
-            # Socket is confused. Close and remove it.
+            print "\nW: No response from server, retrying... (",retry_relay," / ",RELAY_RETRIES,")"
             client.setsockopt(zmq.LINGER, 0)
             client.close()
             poll.unregister(client)
-            #retries_left -= 1
-            #if retries_left == 0:
-            #    print "E: Server seems to be offline, abandoning"
-            #    break
-            print "I: Reconnecting"
-            # Create new connection
+            
+            retry_relay += 1
+            retry_max   += 1
+            
+            if MAX_RETRIES is not False and retry_max == MAX_RETRIES:
+                print "E: Server(s) seem to be offline, max attempts reached, terminating"
+                cont = False
+                break
+            elif retry_relay == RELAY_RETRIES:  # we've reachec max retry attempts for this relay  
+                if (relay == NUM_RELAYS-1): # if we're at te last relay loop around
+                    print "E: Server seems to be offline, switching (looping)"
+                    relay       = 0
+                    retry_relay = 0
+                else:
+                    print "E: Server seems to be offline, switching"
+                    relay      += 1
+                    retry_relay = 0
+
+            print "I: Reconnecting (relay: ",relay,")"
             client = context.socket(zmq.SUB)
             client.setsockopt(zmq.SUBSCRIBE, "")
-            client.connect(RELAYS[0])
+            client.connect(RELAYS[relay])
             poll.register(client, zmq.POLLIN)
-		
-    
+
 def worker(job_json):
-    """
-    For every incoming message, this worker function is called. Be extremely
-    careful not to do anything CPU-intensive here, or you will see blocking.
-    Sockets are async under gevent, so those are fair game.
-    """
-    
     '''
     todo:   look into logging to files per type id
             recurse into rowsets: every feed is not necessarily 1 typeID (though it usually is)
     '''
-    global s;
+    global utd;
     
     if REGIONS is not False:
         json_data  = open(REGIONS)
-        regionDict = json.load(json_data)
+        regionDict = simplejson.load(json_data)
         json_data.close()
-    else:
-        pass
     
     # Receive raw market JSON strings.
     market_json = zlib.decompress(job_json);
-
-    # Un-serialize the JSON data to a Python dict.
     market_data = simplejson.loads(market_json);
     
     # Gather some useful information
@@ -197,7 +189,7 @@ def worker(job_json):
                 if (DEBUG):
                         print "\nCached data found (result type: ",resultType,")!\n\tNew date: "+str(datetime.fromtimestamp(generatedAt))+"\n\tCached date: "+ str(datetime.fromtimestamp(cachedate));
                 if (generatedAt < cachedate):
-                    s += 1
+                    utd += 1
                     if (DEBUG):
                         print "\t\tSKIPPING";
                     return '';
